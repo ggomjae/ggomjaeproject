4fa4de62efc09fcd4f7b7f5ce55c52eebca1133c
ggomjae <ggomjae@naver.com>
2021-02-08 22:50:19 +0900

    spring batch 경우에 따른 step 실행

---
+++ src/main/java/com/ggomjae/project/job/DeciderJobConfiguration.java
@@ -0,0 +1,91 @@
+package com.ggomjae.project.job;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.flow.FlowExecutionStatus;
+import org.springframework.batch.core.job.flow.JobExecutionDecider;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Random;
+
+
+@Configuration
+@RequiredArgsConstructor
+public class DeciderJobConfiguration {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job deciderJob() {
+        return jobBuilderFactory.get("deciderJob")
+                .start(startStep())
+                .next(decider()) // 홀수 | 짝수 구분
+                .from(decider()) // decider의 상태가
+                .on("ODD") // ODD라면
+                .to(oddStep()) // oddStep로 간다.
+                .from(decider()) // decider의 상태가
+                .on("EVEN") // ODD라면
+                .to(evenStep()) // evenStep로 간다.
+                .end() // builder 종료
+                .build();
+    }
+
+    @Bean
+    public Step startStep() {
+        return stepBuilderFactory.get("startStep")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println(">>>>> Start!");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step evenStep() {
+        return stepBuilderFactory.get("evenStep")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println(">>>>> 짝수입니다.");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step oddStep() {
+        return stepBuilderFactory.get("oddStep")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println(">>>>> 홀수입니다.");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public JobExecutionDecider decider() {
+        return new OddDecider();
+    }
+
+    public static class OddDecider implements JobExecutionDecider {
+
+        @Override
+        public FlowExecutionStatus decide(JobExecution jobExecution, StepExecution stepExecution) {
+            Random rand = new Random();
+
+            int randomNumber = rand.nextInt(50) + 1;
+            System.out.println("랜덤숫자 : ");
+            System.out.println(randomNumber);
+
+            if(randomNumber % 2 == 0) {
+                return new FlowExecutionStatus("EVEN");
+            } else {
+                return new FlowExecutionStatus("ODD");
+            }
+        }
+    }
+}
--- src/main/java/com/ggomjae/project/job/SimpleJobConfiguration.java
+++ src/main/java/com/ggomjae/project/job/SimpleJobConfiguration.java
@@ -4,8 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,15 +20,26 @@ public class SimpleJobConfiguration {
     @Bean
     public Job simpleJob() {
         return jobBuilderFactory.get("simpleJob")
-                .start(simpleStep1())
+                .start(simpleStep1(null))
                 .build();
     }
 
+    /*
+        A job instance already exists and is complete for parameters={requestDate=20210208}.
+        If you want to run this job again, change the parameters.
+
+        같은 파라메터로 실행시킬수 없어요.
+
+        즉, 동일한 Job이 Job Parameter가 달라지면 그때마다 BATCH_JOB_INSTANCE에 생성되며,
+        동일한 Job Parameter는 여러개 존재할 수 없다.
+     */
+
     @Bean
-    public Step simpleStep1() {
+    @JobScope
+    public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate) {
         return stepBuilderFactory.get("simpleStep1")
                 .tasklet((contribution, chunkContext) -> {
-                    System.out.println("------Step1");
+                    System.out.println(requestDate);
                     return RepeatStatus.FINISHED;
                 })
                 .build();
